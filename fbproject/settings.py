21952ced323d4580e40990aacb5766ef3a506a3d
hasnapulipra <hasnapulipra@gmail.com>
2022-08-01 19:12:21 +0530

    msg

--- fbproject/settings.py
+++ fbproject/settings.py
@@ -130,7 +130,8 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
